146362cb5d7091f8f2a517d720a3a24cee609da7
Nursidiq97 <sidiqnur363@gmail.com>
2023-03-17 15:33:19 +0700

    Descending

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 console.log("Test");
+// Declare and initialize original array
+var marks = [12, 25, 31, 23, 75, 81, 100];
+
+// Print Before Sorting Array
+console.log("Original Array");
+console.log(marks);
+
+// Call sort function
+marks.sort();
+
+console.log("After Sorting in Ascending Order");
+
+// Print Sorted Numeric Array
+console.log(marks);
