8289cc17c592eca8c3232b4eb76609f83caade5d
adlihardiyanto <testingerkamv2@gmail.com>
2023-03-17 15:11:22 +0700

    Update Sort Javascript

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("Test");
+// console.log("Test");
+// Function Sort - Adli
+var numArray = [11, 16, 18, 20, 26, 50, 100, 1000, 81, 75];
+numArray.sort(function(a, b) {
+    return a - b;
+  });
+  
+console.log(numArray);
