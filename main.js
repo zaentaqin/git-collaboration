2f85b4616938fc3d24903e90be4cdda8c0506ec4
aridwiprayogo <kisamberbledek@yahoo.com>
2023-03-17 15:13:04 +0700

    solving anagram problem in js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log("Test");
+
+const isAnagram = function (s, t) {
+    if(typeof(s) === "string") {
+        const s1 = s.split('').sort().join('');
+        const s2 = t.split('').sort().join('');
+
+        return (s1 === s2)
+    }
+};
+
+console.log(isAnagram("maksud", "muskud"));
