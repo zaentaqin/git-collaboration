2e564d5e52eb82df149518780e7c60a385cc61ed
zaentaqin <zaen.taqin2@gmail.com>
2023-03-17 15:03:15 +0700

    add fizzbuzz

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 console.log("Test");
+
+//fizzbuzz by zen
+function fizzbuzz() {
+  for (var i = 1; i < 30; i++) {
+    if (i % 15 == 0) console.log("FizzBuzz");
+    if (i % 5 == 0) console.log("Fizz");
+    if (i % 3 == 0) console.log("Buzz");
+    else console.log(i);
+  }
+}
+
+fizzbuzz();
