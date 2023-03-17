28918389d3caf3e3837e8cefa40e8c1d37eaa209
Hamdan Irfan Wijaya <ifan5570@gmail.com>
2023-03-17 15:08:29 +0700

    func palindrom javascript

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 console.log("Test");
+
+function isPalindrome(str) {
+    str = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, ""); // mengubah semua huruf menjadi lowercase dan menghapus karakter non-alphanumeric
+    var len = str.length;
+    for (var i = 0; i < len / 2; i++) {
+        if (str[i] !== str[len - 1 - i]) {
+            return "Bukan Palindrom";
+        }
+    }
+    return "Termasuk Palindrom";
+};
+
+console.log(isPalindrome("civic"));
+console.log(isPalindrome("mana anam"));
+console.log(isPalindrome("turbo"));
