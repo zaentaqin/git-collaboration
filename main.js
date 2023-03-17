f5cb9745a57cb23c36889fe69204dadc3c1f9a50
lanastrio <maulanasatrio2401@gmail.com>
2023-03-17 15:13:22 +0700

    jancok

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Test");
+//console.log("Test");
+let array = [5,9,6,7,4]
+function arrayMax() {
+    return array.reduce((a, b)=> Math.max(a, b));
+}
+function arrayMin() {
+    return array.reduce((a, b)=> Math.min(a, b));
+}
+
+console.log('nilai Min', arrayMin())
+console.log('nilai Max', arrayMax())
